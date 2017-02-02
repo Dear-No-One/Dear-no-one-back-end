eecc5f7ef87abdcb76fa4c7f1f7be848acbd57f1
ZacJocelyn <zacjocelyn@gmail.com>
2017-02-02 15:23:05 -0700

    post done

--- migrations/20170202150538_01_member.js
+++ migrations/20170202150538_01_member.js
@@ -2,14 +2,14 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('member', (table) =>{
     table.increments();
-    table.text('username').notNullable();
+    table.text('username').notNullable().unique();
     table.text('email').unique().notNullable();
     table.text('password').notNullable();
     table.date('date_created').notNullable();
     table.boolean('is_active').notNullable().defaultTo(true);
     table.text('bio');
     table.text('profile_pic');
-    table.text('blog_url');
+    table.text('blog_url').unique();
     table.text('facebook');
     table.text('twitter');
     table.text('instagram');
--- migrations/20170202151736_02_post.js
+++ migrations/20170202151736_02_post.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('post', (table) =>{
+    table.increments();
+    table.text('title').notNullable();
+    table.text('body').notNullable();
+    table.text('post_url').notNullable();
+    table.date('date_posted').notNullable();
+    table.integer('member_id').references('member.id').unsigned().onDelete('cascade');
+    table.integer('category_id').references('category.id').unsigned().onDelete('cascade');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('post')
 };
