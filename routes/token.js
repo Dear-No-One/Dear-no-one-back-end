ef33014e393eb1f44e36d87b74155df1b440f5f1
ZacJocelyn <zacjocelyn@gmail.com>
2017-02-15 16:23:59 -0700

    we can log in

--- routes/token.js
+++ routes/token.js
@@ -4,35 +4,57 @@ var bcrypt = require('bcrypt');
 var jwt = require('jsonwebtoken');
 var config = require('../config')
 var knex = require('../db/knex');
+var authHelpers = require('../auth/_helpers')
+var localAuth = require('../auth/local')
+// var app = express();
 
-var app = express();
+// app.set('superSecret', config.secret);
 
-app.set('superSecret', config.secret);
+// router.post('/', function(req, res, next) {
+//   console.log(req.body);
+//
+//   return knex('member').where('email', req.body.auth.email).first()
+//       .then(function(member) {
+//         console.log(member);
+//         console.log(req.body.auth.password);
+//         if(bcrypt.compareSync(req.body.auth.password, member.password)) {
+//           console.log('yay');
+//           // var token = jwt.sign(member, 'shh');
+//           var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+//           console.log(token);
+//           res.json({
+//             token:token
+//            });
+//
+//       } else {
+//          next(new Error('Invalid Signin'))
+//       }})
+//
+//
+//
+//
+// });
 
-router.post('/', function(req, res, next) {
-  console.log(req.body);
-
-  return knex('member').where('email', req.body.email).first()
-      .then(function(member) {
-        console.log(member);
-        console.log(req.body.password);
-        if(bcrypt.compareSync(req.body.password, member.password)) {
-          console.log('yay');
-          var token = jwt.sign(member, app.get('superSecret'));
-          res.json({
-            success:true,
-            message:"enjoy your bbd",
-            token:token
-
-           });
-
-      } else {
-         next(new Error('Invalid Signin'))
-      }})
-
-
-
-
+router.post('/', (req, res, next) => {
+  const email = req.body.auth.email;
+  const password = req.body.auth.password;
+  return authHelpers.getUser(email)
+  .then((response) => {
+    authHelpers.comparePass(password, response.password);
+    return response;
+  })
+  .then((response) => { return localAuth.encodeToken(response); })
+  .then((token) => {
+    res.status(200).json({
+      status: 'success',
+      token: token
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error'
+    });
+  });
 });
 
 module.exports = router;
