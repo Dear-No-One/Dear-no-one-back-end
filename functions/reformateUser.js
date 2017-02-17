34e31cbfc0b5d047018a0b243030fe131b1e0212
ZacJocelyn <zacjocelyn@gmail.com>
2017-02-16 19:16:32 -0700

    wow

---
+++ functions/reformateUser.js
@@ -0,0 +1,33 @@
+function reformMember (member) {
+    const reformatted = [];
+    const memberById = {};
+    member.forEach(member => {
+        if(memberById[member.memberId]){
+            memberById[member.memberId].blogs.push(member.postId);
+    }
+        else {
+        memberById[member.memberId] = {
+            memberId: member.memberId,
+            username: member.username,
+            email: member.email,
+            password: member.password,
+            bio: member.bio,
+            template: member.template,
+            theme: member.theme,
+            profilePic: member.profilePic,
+            blogPic: member.blogPic,
+            facebook: member.facebook,
+            twitter: member.twitter,
+            instagram: member.instagram,
+            blogs: [member.postId]
+        };
+        reformatted.push(memberById[member.memberId]);
+        }
+    });
+    return reformatted;
+}
+
+
+module.exports = {
+    reformMember
+};
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var reform = require('../functions/reformateUser').reformMember;
 
 /* GET users listing. */
 var knex = require('../db/knex');
 
-router.get('/', function(req, res, next) {
-    return knex("member")
+// router.get('/', function(req, res, next) {
+//     return knex("member")
+//         .then(data => {
+//             var result = {
+//                 users: data
+//             };
+//             res.json(result);
+//         });
+// });
+
+router.get('/', (req, res, next) => {
+    knex('member')
+        .select(
+            'member.id as memberId',
+            'member.username',
+            'member.email',
+            'member.password',
+            'member.bio',
+            'member.template',
+            'member.theme',
+            'member.profilePic',
+            'member.blogPic',
+            'member.facebook',
+            'member.twitter',
+            'member.instagram',
+            'post.id as postId')
+        .join('post', 'post.memberId', 'member.id')
         .then(data => {
-            var result = {
-                users: data
-            };
-            res.json(result);
+          const reformatted = reform(data)
+            res.json(reformatted);
+            // res.json(reformatted);
         });
 });
 
